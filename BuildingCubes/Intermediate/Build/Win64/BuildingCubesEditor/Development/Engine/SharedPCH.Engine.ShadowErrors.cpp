02dbc7f3fc09cf8f2a660302607d0cc39b75a377
VitIgVit <77493176+VitIgVit@users.noreply.github.com>
2022-08-25 17:29:35 +0300

    add fix

--- BuildingCubes/Intermediate/Build/Win64/BuildingCubesEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30146
-#include "C:/Users/Vetal/Documents/Unreal Projects/BuildingCubes/BuildingCubes/Intermediate/Build/Win64/BuildingCubesEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
